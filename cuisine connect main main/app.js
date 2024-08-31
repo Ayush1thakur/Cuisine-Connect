ae19ecf39b87be931c4e04b5691cec7a3067d89a
neeshu20 <125849680+neeshu20@users.noreply.github.com>
2024-08-31 23:10:18 +0530

    Add files via upload

--- cuisine connect main main/app.js
+++ cuisine connect main main/app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const fs = require("fs");
 const fooddatabase=require("./food.json");
 const multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
+const uuid=uuidv4();
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
@@ -19,29 +21,35 @@ const storage = multer.diskStorage({
         cb(null, filename);
     }
 });
-
-app.use(express.static('public'));
-
 const singleUpload = multer({ storage }).single("image");
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.render('food', { foods: fooddatabase });
- });
-
-app.get("/admin", (req, res) => {
-    res.sendFile("admin.html", { root: path.join(__dirname, "public/html") });
+let HeaderContent=["Home","Menu","About","Contact"];
+app.get('/Home', (req, res) => {
+   res.render('food', { foods: fooddatabase, header:HeaderContent});
 });
 
+app.get('/Menu', (req, res) => {
+   res.render('menu', { foods: fooddatabase, header:HeaderContent});
+});
+
+
+app.get("/admin", (req, res) => {
+   res.render('AddFood');
+});
+
+// get menu
 app.post("/admin", singleUpload, (req, res, next) => {
-   // Check if file was uploaded
+
    if (!req.file) {
       return res.status(400).send({ message: "No file uploaded" });
    }
 
    let domainName = "http://localhost:1000/FoodImage/";
-   let file = req.file; // Check if the file is present
+   let file = req.file; 
    let { name, price } = req.body;
 
    if (!name || !price) {
@@ -55,10 +63,10 @@ app.post("/admin", singleUpload, (req, res, next) => {
       return res.status(409).send({ message: "Food item already exists" });
    } else {
       let newFood = {
-         id: new Date().getTime(),
+         id: uuid,
          name,
          price,
-         image: file.filename // Use file.filename for proper path
+         image: file.filename 
       };
 
       database.push(newFood);
@@ -67,16 +75,79 @@ app.post("/admin", singleUpload, (req, res, next) => {
          if (err) {
             return next(err);
          }
-         res.status(201).send({ name: newFood.name, message: "New food created" });
+         res.status(201).send({ name: newFood.name, message: "New food item added" });
       });
    }
 });
 
-2
+app.delete("/adminDel/:id", (req, res, next) => {
+   let database = fooddatabase;
+   let foodId = parseInt(req.params.id); 
 
+   let index = database.findIndex((item) => item.id === foodId);
 
+   if (index === -1) {
+       return res.status(404).send({ message: "Food item not found" });
+   } else {
+       let removedItem = database.splice(index, 1);
+       fs.writeFile("./food.json", JSON.stringify(database), (err) => {
+           if (err) {
+               return next(err);
+           }
+           res.status(200).send({ message: "Food item deleted", item: removedItem[0] });
+       });
+   }
+});
 
+app.put("/admin/:id", (req, res, next) => {
+   let database = fooddatabase;
+   let foodId = parseInt(req.params.id); 
+   let { name, price, image } = req.body;
 
+   let index = database.findIndex((item) => item.id === foodId);
+
+   if (index === -1) {
+       return res.status(404).send({ message: "Food item not found" });
+   } else {
+       database[index] = {
+           id: foodId, 
+           name: name || database[index].name, 
+           price: price || database[index].price,
+           image: image || database[index].image
+       };
+
+       fs.writeFile("./food.json", JSON.stringify(database), (err) => {
+           if (err) {
+               return next(err);
+           }
+           res.status(200).send({ message: "Food item updated", item: database[index] });
+       });
+   }
+});
+
+app.patch("/admin/:id", (req, res, next) => {
+   let database = fooddatabase;
+   let foodId = req.params.id;  // Use UUID (string) instead of parseInt
+   let { name, price, image } = req.body;
+
+   let index = database.findIndex((item) => item.id === foodId);
+
+   if (index === -1) {
+       return res.status(404).send({ message: "Food item not found" });
+   } else {
+       // Only update the fields that are provided in the request body
+       if (name) database[index].name = name;
+       if (price) database[index].price = price;
+       if (image) database[index].image = image;
+
+       fs.writeFile("./food.json", JSON.stringify(database), (err) => {
+           if (err) {
+               return next(err);
+           }
+           res.status(200).send({ message: "Food item updated", item: database[index] });
+       });
+   }
+});
 
  
 const PORT = 1000;
